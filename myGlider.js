4545e5d3d73c346215b48a0c6654b52c200404a1
K.Rajendra <51524384+Rajek88@users.noreply.github.com>
2021-04-10 17:20:21 +0530

    Create myGlider.js
    for my apps

---
+++ myGlider.js
@@ -0,0 +1,11 @@
+(function ($) {
+      "use strict";
+
+      // PROJECT CAROUSEL
+      $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        margin: 10,
+        nav: true,
+      });
+    })(jQuery);
